ba166518e8285cebe222ab2358b28d09f56058f2
Rishabh Arora <rishabharora1780@gmail.com>
2018-07-17 23:08:58 +0530

    About tab added

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,8 @@
 document.getElementsByClassName("home-teaser")[0].addEventListener("click", function(){
 	window.location = "https://www.youtube.com/watch?v=oWDZY6y7ED8";
 }); 
+
+document.getElementsByClassName("about-download")[0].addEventListener("click", function(){
+	window.location = "https://www.youtube.com/watch?v=oWDZY6y7ED8";
+}); 
+
