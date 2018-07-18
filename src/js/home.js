aa303d84cdb6941c5d3402df1e4b186c8d184f3d
Rishabh Arora <rishabharora1780@gmail.com>
2018-07-18 12:38:46 +0530

    days countdown added

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,13 @@ document.getElementsByClassName("about-download")[0].addEventListener("click", f
 	window.location = "https://www.youtube.com/watch?v=oWDZY6y7ED8";
 }); 
 
+var DDay =  '2018-10-31';
+function getTimeRemaining(day){
+  var date = Date.parse(day) - Date.parse(new Date());
+  var days = Math.floor(date/(1000*60*60*24));
+  return days;
+}
+
+var x = getTimeRemaining(DDay);
+console.log(x);
+document.getElementById("days-countdown").innerHTML = x + ' days to go';
