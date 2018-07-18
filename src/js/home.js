5b5eb701b03cd048578a4c1c1f30c9d633a5b945
Rishabh Arora <rishabharora1780@gmail.com>
2018-07-18 13:44:47 +0530

    added social media

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,16 @@
+"use strict";
+
+//teaser
 document.getElementsByClassName("home-teaser")[0].addEventListener("click", function(){
 	window.location = "https://www.youtube.com/watch?v=oWDZY6y7ED8";
 }); 
 
+//brochure
 document.getElementsByClassName("about-download")[0].addEventListener("click", function(){
 	window.location = "https://www.youtube.com/watch?v=oWDZY6y7ED8";
 }); 
 
+//days to go
 var DDay =  '2018-10-31';
 function getTimeRemaining(day){
   var date = Date.parse(day) - Date.parse(new Date());
@@ -16,3 +21,15 @@ function getTimeRemaining(day){
 var x = getTimeRemaining(DDay);
 console.log(x);
 document.getElementById("days-countdown").innerHTML = x + ' days to go';
+
+//social media
+var social = document.getElementsByClassName("social-icon");
+social[0].addEventListener("click", function(){
+	window.location = "https://www.facebook.com";
+});
+social[1].addEventListener("click", function(){
+	window.location = "https://www.instagram.com";
+});
+social[2].addEventListener("click", function(){
+	window.location = "https://www.twitter.com";
+});
