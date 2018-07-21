0200a0daf52373e48080390da70b9b874e0ff15c
Rishabh Arora <rishabharora1780@gmail.com>
2018-07-21 15:09:04 +0530

    countdown updated and colors changed

--- src/js/home.js
+++ src/js/home.js
@@ -11,24 +11,31 @@ document.getElementsByClassName("about-download")[0].addEventListener("click", f
 }); 
 
 //days to go
-var DDay =  "2018-10-31";
+var DDay =  "2018-10-27";
 function getTimeRemaining(day){
 	var date = Date.parse(day) - Date.parse(new Date());
 	var days = Math.floor(date/(1000*60*60*24));
+	if(days<=0){
+		document.getElementById("days-countdown").style.display = "none";		
+	}
 	return days;
 }
 
 var x = getTimeRemaining(DDay);
-document.getElementById("days-countdown").innerHTML = x + " days to go";
+if(x==1)
+	document.getElementById("days-countdown").innerHTML = x + " day to go";
+else
+	document.getElementById("days-countdown").innerHTML = x + " days to go";
+
 
 //social media
 var social = document.getElementsByClassName("social-icon");
 social[0].addEventListener("click", function(){
-	window.location = "https://www.facebook.com";
+	window.location = "https://www.facebook.com/oasis.bitspilani/";
 });
 social[1].addEventListener("click", function(){
-	window.location = "https://www.instagram.com";
+	window.location = "https://www.instagram.com/bitsoasis/?hl=en";
 });
 social[2].addEventListener("click", function(){
-	window.location = "https://www.twitter.com";
+	window.location = "https://twitter.com/bitsoasis?lang=en";
 });
