bb883529de00fc30f54eef2e84ed81b5102dedfd
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-07-29 00:10:50 +0530

    Dynamically render images, hide all except first

--- src/glitch/glitch.js
+++ src/glitch/glitch.js
@@ -6,49 +6,60 @@ function init () {
 	const IMAGES = [
 		{
 			name: "1.png",
-			title: "punk"
+			title: "punk",
+			require: () => require("../assets/imgs/1.png")
 		},
 		{
 			name: "2.png",
-			title: "aghori"
+			title: "aghori",
+			require: () => require("../assets/imgs/2.png")
 		},
 		{
 			name: "3.png",
-			title: "goth"
+			title: "goth",
+			require: () => require("../assets/imgs/3.png")
 		},
 		{
 			name: "4.png",
-			title: "lgbt"
+			title: "lgbt",
+			require: () => require("../assets/imgs/4.png")
 		},
 		{
 			name: "5.png",
-			title: "rock"
+			title: "rock",
+			require: () => require("../assets/imgs/5.png")
 		},
 		{
 			name: "6.png",
-			title: "hippie"
+			title: "hippie",
+			require: () => require("../assets/imgs/6.png")
 		}
 	];
 
-	const BASE_PATH = "../assets/imgs/";
 	const CONTAINER = document.getElementById("container");
 
-	IMAGES.map(obj => createImages(obj));
+	IMAGES.map((obj, index) => createImages(obj, index));
 
 
 	// function to create images in HTML
-	function createImages (obj) {
+	function createImages (obj, index) {
 		let div = document.createElement("div");
 		div.classList.add("image");
 
 		let img = document.createElement("img");
-		img.setAttribute("src", require(BASE_PATH+obj.name));
+		img.setAttribute("src", obj.require());
 		img.setAttribute("alt", obj.title);
 		div.appendChild(img);
 		
 		let h1 = document.createElement("H1");
 		h1.innerHTML = obj.title;
 		div.appendChild(h1);
+
+		// display none if not first image
+		if(index!=0) {
+			div.style.display = 'none';
+			div.style.opacity = 0;
+		}
 
 		CONTAINER.appendChild(div);
 	}
