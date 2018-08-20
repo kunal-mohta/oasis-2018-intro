e5a019399e03265e30f2e4ba8c5afbfdbfe8def9
Laksh Singla <lakshsingla@gmail.com>
2018-08-20 23:12:28 +0530

    Video analytics added

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,22 +1,28 @@
+import axios from "axios";
+
 (function videoLog(){
-	//const apiRoot = "https://bits-oasis.org/2018/analytics/views/";
+	const apiRoot = "https://bits-oasis.org/2018/analytics/views/";
 	const vids = 
 		[
 			{
 				className: "analytics-teaser18", 
 				videoName: "teaser18",
+				watched: false
 			},
 			{
 				className: "analytics-aftermovie17", 
 				videoName: "aftermovie17",
+				watched: false
 			},
 			{
 				className: "analytics-teaser17", 
 				videoName: "teaser17",
+				watched: false
 			},
 			{
 				className: "analytics-aftermovie16", 
 				videoName: "aftermovie16",
+				watched: false
 			}
 		];
 	vids.forEach(vidObj => {
@@ -24,7 +30,23 @@
 		const classEl = document.getElementsByClassName(className);
 		Array.from(classEl).forEach(el => {
 			el.addEventListener('click', () => {
-				console.log(videoName);
+				console.log("clicked");
+				if(vidObj.watched) return;
+				vidObj.watched = true;
+				axios({
+					method: "post",
+					url: apiRoot,
+					data: JSON.stringify({
+						name: videoName	
+					}),
+					headers: {
+						"Access-Control-Allow-Origin": "*",
+						"Content-Type": "application/json"
+					}
+				}).then(response => {
+					console.log("here")
+					console.log(response)	
+				}).catch(err => console.log(err))
 			});	
 		})
 	});
