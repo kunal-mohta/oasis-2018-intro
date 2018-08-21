1bdc7518fd59fd034c6d30aed66351db83838465
Laksh Singla <lakshsingla@gmail.com>
2018-08-21 23:10:05 +0530

    home viewtimer added

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -51,5 +51,50 @@ import axios from "axios";
 
 
 (function homeScreenLog(){
+
+	const apiRoot = "https://bits-oasis.org/2018/analytics/viewtimer/";
 	
+	let secondsUntilNavigate = -3.5;
+	window.addEventListener('load', () => setInterval(
+		() => {++secondsUntilNavigate},
+		1000
+	))
+	
+	const searchTabs = [
+		...Array.from(document.querySelectorAll('#nav-list li')),
+		...Array.from(document.querySelectorAll('#mobile-nav-list li')),
+	]; 
+
+	const excludeIds = [
+		'home-nav', 'mobile-home-nav'
+	];
+
+	let changed = false;
+
+	searchTabs
+		.filter(tab => !excludeIds.some(excludeId => tab.id === excludeId))
+		.forEach(tab => tab.addEventListener('click',
+		() => {
+			//If already navigated away
+			if(changed) return;
+
+			changed = true;
+
+			console.log(changed);
+
+			axios({
+					method: "post",
+					url: apiRoot,
+					data: JSON.stringify({
+						seconds_viewed: secondsUntilNavigate
+					}),
+					headers: {
+						"Access-Control-Allow-Origin": "*",
+						"Content-Type": "application/json"
+					}
+				}).then(()=> {console.log(secondsUntilNavigate)})
+					.catch((err) => {console.log(err)})
+			
+		}));
+
 })();
